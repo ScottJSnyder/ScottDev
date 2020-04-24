317a319dad1c58455e83cb0714569ee25f52d748
Scott Snyder <ssnyder@optensity.onmicrosoft.com>
2020-04-24 10:36:23 -0400

    Inital commit

---
+++ src/DxComponent.js
@@ -0,0 +1,85 @@
+import React from "react";
+ 
+import "devextreme/dist/css/dx.common.css";
+import "devextreme/dist/css/dx.light.css";
+import "./DxComponent.css";
+ 
+import { Drawer } from "devextreme-react/drawer";
+import { Toolbar, Item } from "devextreme-react/toolbar";
+import NavigationList from "./NavigationList";
+ 
+//import { Router, Route } from "react-router-dom";
+import {
+    BrowserRouter,
+    Router,
+    Link,
+    Route,
+    Switch
+} from 'react-router-dom';
+import Home from "./Home";
+import Apps from "./Apps";
+import Artifacts from "./Artifacts";
+import Deployments from "./Deployments";
+import Jobs from "./Jobs";
+
+import history from "./history";
+ 
+class DxComponent extends React.Component {
+    constructor(props) {
+        super(props);
+ 
+        this.state = {
+            isDrawerOpen: false
+        };
+        this.buttonOptions = {
+            icon: "menu",
+            onClick: () => {
+                this.setState({ isDrawerOpen: !this.state.isDrawerOpen });
+            }
+        };
+    }
+ 
+    renderList = () => {
+        const stateHandler = (newState) => this.setState(newState);
+        return (
+            <NavigationList stateHandler={stateHandler} />
+        );
+    }
+ 
+    render() {
+        return (
+
+            <React.Fragment>
+                <Toolbar id="toolbar">
+                    <Item 
+                        widget="dxButton" 
+                        options={this.buttonOptions} 
+                        location="before" /> 
+                </Toolbar> 
+                
+                <Drawer
+                    minSize={37}
+                    height={600}
+                    revealMode="expand"
+                    openedStateMode="overlap"
+                    render={this.renderList}
+                    opened={this.state.isDrawerOpen} >
+                    <div id="view">
+                    <Router history={history}>
+                            <div>
+                                <Route exact path="/" component={Home} />
+                                <Route exact path="/home" component={Home} />
+                                <Route exact path="/apps" component={Apps} />
+                                <Route exact path="/artifacts" component={Artifacts} />
+                                <Route exact path="/deployments" component={Deployments} />
+                                <Route exact path="/jobs" component={Jobs} />
+                            </div>
+                            </Router>
+                    </div>
+                </Drawer>
+                
+            </React.Fragment>
+        );
+    }
+}
+export default DxComponent;
---
+++ target/appsymphony-console/precache-manifest.243bf8c0868320e478667395d9579c9b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec5b9fea24d6ce4b4b5e5615b1eb7ed7",
+    "url": "/appsymphony-console/index.html"
+  },
+  {
+    "revision": "e631bba6deba981c6682",
+    "url": "/appsymphony-console/static/css/2.f4d24185.chunk.css"
+  },
+  {
+    "revision": "03ab6124c8810b58c4be",
+    "url": "/appsymphony-console/static/css/main.1989bbbd.chunk.css"
+  },
+  {
+    "revision": "e631bba6deba981c6682",
+    "url": "/appsymphony-console/static/js/2.078ffaaf.chunk.js"
+  },
+  {
+    "revision": "6a8e765d7a6e11ca5071a62bdf07077d",
+    "url": "/appsymphony-console/static/js/2.078ffaaf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03ab6124c8810b58c4be",
+    "url": "/appsymphony-console/static/js/main.4b5b2617.chunk.js"
+  },
+  {
+    "revision": "5f8c63a60435d2126640",
+    "url": "/appsymphony-console/static/js/runtime-main.0f47c95f.js"
+  },
+  {
+    "revision": "1286a19fc578e6b5a92e6dcb744dec16",
+    "url": "/appsymphony-console/static/media/dxicons.1286a19f.woff2"
+  },
+  {
+    "revision": "147eb6fe61ff3af12cd8763022ae7f24",
+    "url": "/appsymphony-console/static/media/dxicons.147eb6fe.ttf"
+  },
+  {
+    "revision": "2b145cefd95f699e2eecc965fb38c8a8",
+    "url": "/appsymphony-console/static/media/dxicons.2b145cef.woff"
+  }
+]);
